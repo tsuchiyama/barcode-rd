a04ced9af9b2248d2f5948917e3ef213a3448c69
gtsuchiyama <gary.tsuchiyama@gamaninc.com>
2023-09-16 14:48:18 -0500

    2nd commit

--- barcode-rd.py
+++ barcode-rd.py
@@ -11,17 +11,14 @@ detectedBarcodes = decode(img)
 # If not detected then print the message
 if not detectedBarcodes:
 print("Barcode Not Detected or your barcode is blank/corrupted!")
-    else:
-       
-          # Traverse through all the detected barcodes in image
-        for barcode in detectedBarcodes: 
-           
-            # Locate the barcode position in image
-            (x, y, w, h) = barcode.rect
-             
-            # Put the rectangle in image using
-            # cv2 to highlight the barcode
-            cv2.rectangle(img, (x-10, y-10),
+else:
+# Traverse through all the detected barcodes in image
+for barcode in detectedBarcodes:
+# Locate the barcode position in image
+(x, y, w, h) = barcode.rect
+# Put the rectangle in image using
+# cv2 to highlight the barcode
+cv2.rectangle(img, (x-10, y-10),
                           (x + w+10, y + h+10),
                           (255, 0, 0), 2)
              
